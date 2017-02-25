03380c8711beb18da4717be07d9c78f4dda5e4fc
jcrademacher <johnjr5599@gmail.com>
2017-02-25 17:49:26 -0500

    created Provider for redux

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,6 +1,26 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import allReducers from './reducers';
+
+import Button from 'react-bootstrap/lib/Button';
+import ButtonToolbar from 'react-bootstrap/lib/ButtonToolbar';
+
+import AppView from '../../dev/js/components/containers/AppView.jsx';
 
 const App = () => {
-	return null;
+	return (
+		<AppView/>
+	);
 }
+
+// redux store
+const store = createStore(allReducers);
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App/>
+	</Provider>
+	, document.getElementById('app')
+);
