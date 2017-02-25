23df71d261996eec7c8ae239911d0296f4676a41
jcrademacher <johnjr5599@gmail.com>
2017-02-25 14:03:44 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+	entry: './dev/js/index.js',
+	output: { path: './src/pages', filename: 'main.bundle.js'},
+	module: {
+		loaders: [
+			{
+				test: /jsx?$/,
+				loader: 'babel-loader',
+				exclude: /(node_modules|resources)/,
+				query: {
+					presets: ['es2015', 'react']
+				}
+			}
+		]
+	}
+}
