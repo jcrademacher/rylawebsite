5f595159f1826992edd9111be8629871fd2f9652
jcrademacher <johnjr5599@gmail.com>
2017-02-26 10:21:45 -0500

    added background image

--- dev/js/components/containers/AppView.jsx
+++ dev/js/components/containers/AppView.jsx
@@ -1,14 +1,47 @@
 import React from 'react';
-import Navigation from '../components/Navigation.jsx'
+import Navigation from '../components/Navigation.jsx';
+import {pageTheme} from '../../styles.js';
+import StaffImage from '../../../../resources/staff.jpg';
 
+/*
+	AppView Container
+	Holds all necessary components for sitepage
+*/
 export default class AppView extends React.Component {
 	constructor(props) {
 		super(props);
 	}
 
 	render() {
+		var styles = {
+			footerStyle: {
+				backgroundColor: pageTheme.primary,
+				color: "white",
+				height: 100,
+				textAlign: "center"
+			},
+
+			main: {
+				backgroundImage: 'url(' + StaffImage + ')',
+				backgroundRepeat: 'no-repeat',
+				backgroundPosition: 'center',
+				backgroundSize: "100% 100%",
+				height: 100,
+			}
+		}
+
 		return (
-			<Navigation/>
+			<div>
+				<Navigation/>
+				<div style={styles.main}>
+
+				</div>
+				<footer style={styles.footerStyle}>
+					<br/>
+					<p>Copyright &copy; 2017 Jack Rademacher</p>
+					<p>Created using React, Redux, and React-Bootstrap</p>
+				</footer>
+			</div>
 		);
 	}
 }
