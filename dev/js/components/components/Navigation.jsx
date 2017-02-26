c4f9ad0e84e675ca37deeafe0a846feeadd349df
jcrademacher <johnjr5599@gmail.com>
2017-02-26 10:20:52 -0500

    added RYLA District 7780 to Navbar

--- dev/js/components/components/Navigation.jsx
+++ dev/js/components/components/Navigation.jsx
@@ -8,18 +8,14 @@ const Navigation = () => {
 		<Navbar>
 	    <Navbar.Header>
 	      <Navbar.Brand>
-	        <a href="#">React-Bootstrap</a>
+	        <a href="/">RYLA District 7780</a>
 	      </Navbar.Brand>
 	      <Navbar.Toggle />
 	    </Navbar.Header>
 	    <Navbar.Collapse>
-	      <Nav>
-	        <NavItem eventKey={1} href="#">Link</NavItem>
-	        <NavItem eventKey={2} href="#">Link</NavItem>
-	      </Nav>
 	      <Nav pullRight>
-	        <NavItem eventKey={1} href="#">Link Right</NavItem>
-	        <NavItem eventKey={2} href="#">Link Right</NavItem>
+	        <NavItem eventKey={1} href="#">About</NavItem>
+	        <NavItem eventKey={2} href="#">Contact</NavItem>
 	      </Nav>
 	    </Navbar.Collapse>
   	</Navbar>
