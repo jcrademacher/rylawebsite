106233b76fcd4637c7182d2d508daa6d1bf911aa
jcrademacher <johnjr5599@gmail.com>
2017-03-01 17:58:37 -0500

    reducers correctly functions with redux

--- dev/js/reducers/index.js
+++ dev/js/reducers/index.js
@@ -1,3 +1,19 @@
-export default function() {
-	return {};
+import {combineReducers} from 'redux';
+
+var CurrentTabReducer = function(state=null, action) {
+	if(state == null)
+		return 0;					// default currentTab,
+	else {
+		switch(action.type) {
+			case "NAV_TAB_CHANGED":
+				return action.payload;
+				break;
+		}
+	}
 }
+
+// this is essentially what the 'store' looks like
+// the reducers return data that create a new store
+export default combineReducers({
+	currentTab: CurrentTabReducer
+});
