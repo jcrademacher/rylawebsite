5f39e20756fc06ac6c3995a9e8e29a233af15ad3
jcrademacher <johnjr5599@gmail.com>
2017-03-01 17:51:47 -0500

    added images importing js file

---
+++ dev/js/images.js
@@ -0,0 +1,29 @@
+import img1 from '../../resources/img1.jpg';
+import img2 from '../../resources/img2.jpg';
+import img3 from '../../resources/img3.jpg';
+import img4 from '../../resources/img4.jpg';
+import img5 from '../../resources/img5.jpg';
+import img6 from '../../resources/img6.jpg';
+import img7 from '../../resources/img7.jpg';
+import img8 from '../../resources/img8.jpg';
+import img9 from '../../resources/img9.jpg';
+import img10 from '../../resources/img10.jpg';
+import img11 from '../../resources/rotary.png';
+import img12 from '../../resources/staff.jpg';
+import img13 from '../../resources/ryla.jpg';
+
+export const images = [
+	img1,
+	img2,
+	img3,
+	img4,
+	img5,
+	img6,
+	img7,
+	img8,
+	img9,
+	img10,
+	img11,
+	img12,
+	img13
+];
