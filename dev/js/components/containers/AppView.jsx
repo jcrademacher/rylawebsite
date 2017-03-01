a80d5016934248c8080ffe30039d900d773c54aa
jcrademacher <johnjr5599@gmail.com>
2017-03-01 17:56:32 -0500

    created functionality for tab switching, and linked with redux reducers and actions

--- dev/js/components/containers/AppView.jsx
+++ dev/js/components/containers/AppView.jsx
@@ -1,27 +1,38 @@
 import React from 'react';
 import Navigation from '../components/Navigation.jsx';
-import Footer from '../components/Footer.jsx';
-import Content from '../components/Content.jsx';
-import Greeting from '../components/Greeting.jsx';
+import ViewHandler from '../components/ViewHandler.jsx';
+
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {navTabChanged} from '../../actions/index.js'
+
+// sends store state into AppView as props
+function mapStateToProps(state) {
+	return {
+		currentTab: state.currentTab
+	};
+}
+
+// links actions with redux to be available as props in AppView
+function mapDispatchToProps(dispatch) {
+	return bindActionCreators({navTabChanged}, dispatch);
+}
 
 /*
 	AppView Container
 	Holds all necessary components for sitepage
 */
-export default class AppView extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-
-	render() {
-		return (
-			<div>
-				<Navigation/>
-				<Content>
-					<Greeting/>
-				</Content>
-				<Footer/>
-			</div>
-		);
-	}
+const AppView = (props) => {
+	return (
+		<div>
+			<Navigation
+				onChange={props.navTabChanged}
+				activeKey={props.currentTab}
+			/>
+			<ViewHandler currentTab={props.currentTab}/>
+		</div>
+	);
 }
+
+// links AppView with redux
+export default connect(mapStateToProps, mapDispatchToProps)(AppView);
