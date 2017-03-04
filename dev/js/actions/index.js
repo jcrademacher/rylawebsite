e879cac864d15250e81b88b182421634345d7d09
jcrademacher <johnjr5599@gmail.com>
2017-03-04 14:39:45 -0500

    added InfoViewReducer

--- dev/js/actions/index.js
+++ dev/js/actions/index.js
@@ -1,8 +1,10 @@
-export const navTabChanged = (currentTab) => {
-	console.log("Navbar Changed, currentTab: " + currentTab);
-
-	return {
+export const navTabChanged = (currentTab) => ({
 		type: "NAV_TAB_CHANGED",
 		payload: currentTab 					// returning an action with type and payload
-	};
-}
+});
+
+// reducer for view of about section (contact, directions, about etc.)
+export const infoViewChanged = (currentInfoView) => ({
+		type: "INFO_VIEW_CHANGED",
+		payload: currentInfoView  // string i.e., "about", "contact", "directions", etc.
+});
