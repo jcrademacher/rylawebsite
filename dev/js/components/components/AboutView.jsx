c4cb94d62eaa86c6b11ed3c72345f91e515d0111
jcrademacher <johnjr5599@gmail.com>
2017-03-05 23:08:50 -0500

    added window event listener for screen width adaptibility

--- dev/js/components/components/AboutView.jsx
+++ dev/js/components/components/AboutView.jsx
@@ -1,28 +1,96 @@
 import React from 'react';
 import pageTheme from '../../styles.js';
+import {images} from '../../images.js';
+import {Image} from 'react-bootstrap';
 
-const styles = {
-	left: {
-		float: "left",
-		width: "30%",
-	},
+class AboutView extends React.Component {
+	constructor(props) {
+		super(props);
 
-	right: {
-		float: "right",
-		width: "70%",
-
+		this.state = {
+			windowWidth: window.innerWidth,
+			windowHeight: window.innerHeight
+		};
 	}
-}
 
-const AboutView = (props) => (
-	<div>
-		<div style={styles.left}>
-			{props.children[0]}
-		</div>
-		<div style={styles.right}>
-			{props.children[1]}
-		</div>
-	</div>
-);
+	updateDimensions() {
+		this.setState({
+			windowWidth: window.innerWidth,
+			windowHeight: window.innerHeight
+		});
+	}
+
+	// adds resize event listener
+	componentDidMount() {
+    this.updateDimensions();
+    window.addEventListener("resize", this.updateDimensions.bind(this));
+  }
+
+	// removes resize event listener
+	componentWillUnmount() {
+    window.removeEventListener("resize", this.updateDimensions.bind(this));
+  }
+
+	render() {
+
+		var AboutMenu = this.props.children[0];
+		var About = this.props.children[1];
+
+		const styles = {
+			left: {
+				float: "left",
+				width: "70%",
+			},
+
+			right: {
+				float: "right",
+				width: "30%",
+			},
+
+			image: {
+				textAlign: "center",
+				padding: this.state.windowWidth >= 768 ? 10:60,
+				// conditional, 10 padding if greater, 60 if less
+			}
+		}
+
+		if(this.state.windowWidth >= 768) {
+			return (
+				<div>
+					<br/>
+					<br/>
+					<div style={styles.left}>
+						{About}
+					</div>
+					<div style={styles.right}>
+						<div style={{height: "60vh"}}>
+							<br/><br/>
+							{React.cloneElement(AboutMenu, {dropdown: false})} {/* passes additional props to AboutMenu */}
+						</div>
+						<div style={styles.image}>
+							<Image src={images[12]} responsive/>
+						</div>
+					</div>
+				</div>
+			);
+		}
+		else {
+			return(
+				<div>
+					<br/><br/>
+					<div style={styles.image}>
+						<Image src={images[12]} responsive/>
+					</div>
+					<div style={{textAlign: "center"}}>
+						{React.cloneElement(AboutMenu, {dropdown: true})}
+					</div>
+					<div>
+						{About}
+					</div>
+				</div>
+			);
+		}
+	}
+};
 
 export default AboutView;
