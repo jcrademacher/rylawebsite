ade3c9f4f61303f88e6559d709d533d8c57c9702
jcrademacher <johnjr5599@gmail.com>
2017-03-05 23:07:24 -0500

    added screen size adaptibility, nav turns to dropdown below 768px width

--- dev/js/components/components/AboutMenu.jsx
+++ dev/js/components/components/AboutMenu.jsx
@@ -1,21 +1,59 @@
 import React from 'react';
+import {Nav} from 'react-bootstrap';
+import {NavItem} from 'react-bootstrap';
+import {DropdownButton} from 'react-bootstrap';
+import {MenuItem} from 'react-bootstrap';
 
 const divStyle = {
 	//border: "4px solid black",
 	margin: 10
 };
 
-const AboutMenu = (props) => (
-	<div style={divStyle}>
-		<br/><br/>
-		<ul>
-			<li><a href="javascript:console.log('test')">Test Link</a></li>
-		</ul>
-	</div>
-);
+const AboutMenu = (props) =>  {
+	const handleNavSelect = (key) => {
+		props.infoViewChanged(key);
+	};
 
-const MenuLink = (props) => (
-	<a></a>
-);
+	const navItems = [
+		"About RYLA", "The Principles of Rotary", "Contact", "Directions to Camp Hinds", "FAQ"
+	];
+
+	// returns correct nav items based on prop dropdown
+	// requires a different component for a dropdown
+	const getNavItems = (type) => {
+		if(type == 'nav') {
+			return (
+				// loops through all elements in list
+				<Nav stacked bsStyle="pills" activeKey={props.currentInfoView} onSelect={handleNavSelect}>
+					{navItems.map((title, index) => {
+						return (
+							<NavItem eventKey={index}>{title}</NavItem>
+						);
+					})}
+				</Nav>
+			);
+		}
+		else if(type == 'dropdown') {
+			return (
+				<DropdownButton title={navItems[props.currentInfoView]} onSelect={handleNavSelect}>
+					{navItems.map((title, index) => {
+						return (
+							<MenuItem eventKey={index}>{title}</MenuItem>
+						);
+					})}
+				</DropdownButton>
+			);
+		}
+	}
+
+	//sets dropdown to false if undefined, actual value if not undefined
+	const dropdown = props.dropdown == undefined ? false : props.dropdown;
+
+	return (
+		<div style={divStyle}>
+			{getNavItems(dropdown ? 'dropdown':'nav')}
+		</div>
+	);
+};
 
 export default AboutMenu;
