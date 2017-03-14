9d6fcea3ee18f20d10d00933c440b007fb547cb2
jcrademacher <johnjr5599@gmail.com>
2017-03-14 12:55:45 -0400

    minor

--- dev/js/components/components/InfoPanel.jsx
+++ dev/js/components/components/InfoPanel.jsx
@@ -2,34 +2,6 @@ import React from 'react';
 import {Button} from 'react-bootstrap';
 
 class InfoPanel extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			windowWidth: window.innerWidth,
-			windowHeight: window.innerHeight
-		};
-
-		this.updateDimensions = this.updateDimensions.bind(this);
-	}
-
-	updateDimensions() {
-		this.setState({
-			windowWidth: window.innerWidth,
-			windowHeight: window.innerHeight
-		});
-	}
-
-	// adds resize event listener
-	componentDidMount() {
-    this.updateDimensions();
-    window.addEventListener("resize", this.updateDimensions);
-  }
-
-	// removes resize event listener
-	componentWillUnmount() {
-    window.removeEventListener("resize", this.updateDimensions);
-  }
 
 	render() {
 		var textColor = this.props.textColor;
