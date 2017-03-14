8340aebe67b9526476e7ade9900f1a01bbaf796f
jcrademacher <johnjr5599@gmail.com>
2017-03-14 13:02:54 -0400

    changed to require

--- dev/js/images.js
+++ dev/js/images.js
@@ -1,16 +1,18 @@
-import img1 from '../../resources/img1.jpg';
-import img2 from '../../resources/img2.jpg';
-import img3 from '../../resources/img3.jpg';
-import img4 from '../../resources/img4.jpg';
-import img5 from '../../resources/img5.jpg';
-import img6 from '../../resources/img6.jpg';
-import img7 from '../../resources/img7.jpg';
-import img8 from '../../resources/img8.jpg';
-import img9 from '../../resources/img9.jpg';
-import img10 from '../../resources/img10.jpg';
-import img11 from '../../resources/rotary.png';
-import img12 from '../../resources/staff.jpg';
-import img13 from '../../resources/ryla.jpg';
+var dirname = '/Volumes/EXTERNAL/MyStuff/Bowst/rylawebsite'
+
+var img1 = require('../../resources/img1.jpg');
+var img2 = require('../../resources/img2.jpg');
+var img3 = require('../../resources/img3.jpg');
+var img4 = require('../../resources/img4.jpg');
+var img5 = require('../../resources/img5.jpg');
+var img6 = require('../../resources/img6.jpg');
+var img7 = require('../../resources/img7.jpg');
+var img8 = require('../../resources/img8.jpg');
+var img9 = require('../../resources/img9.jpg');
+var img10 = require('../../resources/img10.jpg');
+var img11 = require('../../resources/rotary.png');
+var img12 = require('../../resources/staff.jpg');
+var img13 = require('../../resources/ryla.jpg');
 
 
 export const images = [
