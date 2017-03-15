4d9c399d920a898b985154ec0491e57f60fb9267
jcrademacher <johnjr5599@gmail.com>
2017-03-15 19:42:03 -0400

    added index.js

---
+++ dev/js/main.index.js
@@ -0,0 +1,26 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import allReducers from './reducers';
+
+import AppView from './components/containers/AppView.jsx';
+
+import { Router, Route, Link, hashHistory} from 'react-router';
+
+const App = () => {
+	return (
+		<AppView/>
+	);
+}
+
+// redux store
+const store = createStore(allReducers);
+
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App/>
+	</Provider>
+	, document.getElementById('app')
+);
