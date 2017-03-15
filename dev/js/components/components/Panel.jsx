82f56401611dabcc0bdf8e6421397587731f798b
jcrademacher <johnjr5599@gmail.com>
2017-03-15 19:40:37 -0400

    added rounded border, shadow

--- dev/js/components/components/Panel.jsx
+++ dev/js/components/components/Panel.jsx
@@ -1,9 +1,15 @@
+import React from 'react';
+
 const Panel = (props) => {
+
+	const border = props.borderWidth + "px solid " + props.borderColor;
 
 	const panelStyle = {
 		backgroundColor: props.backgroundColor,
-		borderRadius: 4,
-		border: {props.borderWidth} + "px solid " + {props.borderColor},
+		borderRadius: 6,
+		border,
+		color: props.textColor,
+		boxShadow: "5px 5px 20px rgb(100, 100, 100)"
 	};
 
 	return (
