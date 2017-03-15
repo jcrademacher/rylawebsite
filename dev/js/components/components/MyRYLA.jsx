5588dbef7f20731e158eab8f217781de5eb06498
jcrademacher <johnjr5599@gmail.com>
2017-03-15 19:40:20 -0400

    added username and password field

--- dev/js/components/components/MyRYLA.jsx
+++ dev/js/components/components/MyRYLA.jsx
@@ -1,37 +1,59 @@
 import React from 'react';
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Grid, Row, Col, Button } from 'react-bootstrap';
 
-const loginPanelStyle = {
-	backgroundColor: "rgb(210, 210, 210)",
-	borderRadius: 10
-};
+import { Link } from 'react-router';
+
+import Panel from '../components/Panel.jsx';
+import FieldGroup from '../components/FieldGroup.jsx';
+
+import pageTheme from '../../styles.js';
 
 const divStyle = {
-	border: "1px solid black",
-	//textAlign: "center",
+	border: "1px solid",
 	marginLeft: 200,
-	marginRight: 200,
-
+	marginRight: 200
 }
-const LoginPanel = (props) => (
-	<div style={loginPanelStyle}>
-		<p>
-			Loradfksdjkfasldfjasjdfasbdvka dkjfa sjdfkjasdfk askdfk asd askdfaksd fjas asd ka fska sf aksdf aksdf as dfhkasd fkashfd kasd
-		</p>
+
+const Login = (props) => (
+	<div style={{textAlign: "left", margin: 18}}>
+		<form>
+			<FieldGroup
+				id="username"
+				type="text"
+				label="Username:"
+			/><br/>
+			<FieldGroup
+				id="password"
+				type="password"
+				label="Password:"
+			/><br/>
+			<Button block bsStyle="success">Log In</Button>
+			<p style={{textAlign: "center"}}>Don't have an account? <Link style={{color: "white"}} to='/register'>Click here.</Link></p>
+		</form>
 	</div>
 );
+
+
 
 const MyRYLA = (props) => {
 	return (
 		<div style={{textAlign: "center"}}>
-			<br/><br/>
+			<br/><br/><br/>
 			<h1>Sign in to your MyRYLA account</h1>
+			<br/>
 			<Row>
-				<Col md={3}/>
-				<Col md={6}>
-					<LoginPanel/>
+				<Col xs={2} sm={3} md={4}/>
+				<Col xs={8} sm={6} md={4}>
+					<Panel
+						borderColor="white"
+						borderWidth="1"
+						backgroundColor={pageTheme.secondary}
+						textColor='white'
+					>
+						<Login/>
+					</Panel>
 				</Col>
-				<Col md={3}/>
+				<Col xs={2} sm={3} md={4}/>
 			</Row>
 		</div>
 	);
