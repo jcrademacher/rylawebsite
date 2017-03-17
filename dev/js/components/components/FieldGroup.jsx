0d40b312a9d609773c93388baf5ee4f58ed2f40c
jcrademacher <johnjr5599@gmail.com>
2017-03-17 17:29:46 -0400

    validationState is bool, added better reusability

--- dev/js/components/components/FieldGroup.jsx
+++ dev/js/components/components/FieldGroup.jsx
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { PropTypes as T } from 'react';
 import {FormGroup, FormControl, ControlLabel, HelpBlock} from 'react-bootstrap';
 
-const FieldGroup = ({id, label, helpLabel, type}) => (
-	<FormGroup controlId={id}>
-    <h3 style={{fontSize: 20}}>{label}</h3>
-    <FormControl type={type} />
-    {helpLabel && <HelpBlock>{helpLabel}</HelpBlock>}
-  </FormGroup>
-);
+export default class FieldGroup extends React.Component {
 
-export default FieldGroup;
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			textValue: ''
+		};
+	}
+
+	onChange(e) {
+		this.setState({ textValue: e.target.value });
+
+		this.props.onChange(e);
+	}
+
+	render() {
+		return (
+			<FormGroup controlId={this.props.id} validationState={this.props.validationState}>
+		    {this.props.label && <h3 style={{fontSize: 20}}>{this.props.label}</h3>}
+		    <FormControl type={this.props.type} placeholder={this.props.placeholder} onChange={(e) => this.onChange(e)}/>
+				{this.props.feedback && <FormControl.Feedback/>}
+		    {this.props.helpLabel && <HelpBlock>{this.props.helpLabel}</HelpBlock>}
+		  </FormGroup>
+		);
+	}
+};
+
+FieldGroup.propTypes = {
+	id: T.string.isRequired,
+	label: T.string,
+	validationState: T.bool,
+	placeholder: T.string,
+	feedback: T.bool,
+	type: T.string.isRequired,
+	onChange: T.func,
+	helpLabel: T.string
+};
+
+FieldGroup.defaultProps = {
+	feedback: false,
+	validationState: null,
+	placeholder: "",
+	onChange: function() {}
+};
