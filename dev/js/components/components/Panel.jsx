5e2dc9dedaad490dad771dc5d9cf1d6bc14ef4ac
jcrademacher <johnjr5599@gmail.com>
2017-03-17 17:30:32 -0400

    reusable, added shadow prop

--- dev/js/components/components/Panel.jsx
+++ dev/js/components/components/Panel.jsx
@@ -4,12 +4,16 @@ const Panel = (props) => {
 
 	const border = props.borderWidth + "px solid " + props.borderColor;
 
+	var backgroundColor = props.backgroundColor == undefined ? "none" : props.backgroundColor;
+	var shadow = props.shadow == undefined || props.shadow == false ? "none" : "5px 5px 20px rgb(100, 100, 100)";
+
 	const panelStyle = {
-		backgroundColor: props.backgroundColor,
+		backgroundColor: backgroundColor,
 		borderRadius: 6,
 		border,
 		color: props.textColor,
-		boxShadow: "5px 5px 20px rgb(100, 100, 100)"
+		boxShadow: shadow,
+		padding: 30
 	};
 
 	return (
