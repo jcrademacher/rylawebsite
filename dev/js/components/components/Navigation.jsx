164015f504f5ba58fdcefc8af1e3160f1339e0de
jcrademacher <johnjr5599@gmail.com>
2017-03-26 21:52:49 -0400

    minor

--- dev/js/components/components/Navigation.jsx
+++ dev/js/components/components/Navigation.jsx
@@ -35,10 +35,10 @@ const Navigation = (props) => {
 	const getActiveKey = () => {
 		switch (props.path) {
 			case '/': return 0;
-			case '/MyRYLA': case '/login': return 1;
+			case '/MyRYLA': return 1;
 			case '/gallery': return 2;
 			case '/about': return 3;
-			default: return 3;
+			default: return 1;
 		}
 	}
 
