d497c14ee4a62196b8889c334717f9f4c642dd39
jcrademacher <johnjr5599@gmail.com>
2017-03-27 22:16:33 -0400

    added WINDOW_WIDTH_UPDATED and WINDOW_HEIGHT_UPDATED actions

--- dev/js/actions/index.js
+++ dev/js/actions/index.js
@@ -1,24 +1,36 @@
-export const navTabChanged = (currentTab) => {
-	if(typeof currentTab != "number") {
-		throw "CurrentTab should be a number, got " + (typeof currentTab);
+export const setProfile = (profile) => {
+	if(typeof profile != "object") {
+		throw "Profile should be an object, got " + (typeof profile);
 		return;
 	}
 
 	return {
-		type: "NAV_TAB_CHANGED",
-		payload: currentTab 					// returning an action with type and payload
+		type: "PROFILE_UPDATED",
+		payload: profile 					// returning an action with type and payload
 	};
 };
 
-// reducer for view of about section (contact, directions, about etc.)
-export const infoViewChanged = (currentInfoView) => {
-	if(typeof currentInfoView != "number") {
-		throw "CurrentTab should be a number, got " + (typeof currentInfoView);
+// the following two actions handling window resizing
+export const setWindowWidth = (width) => {
+	if(typeof width != 'number') {
+		throw 'Width should be a number, got ' + (typeof width);
 		return;
 	}
 
 	return {
-		type: "INFO_VIEW_CHANGED",
-		payload: currentInfoView  // string i.e., "about", "contact", "directions", etc.
-	};
+		type: "WINDOW_WIDTH_UPDATED",
+		payload: width
+	}
+};
+
+export const setWindowHeight = (height) => {
+	if(typeof height != 'number') {
+		throw 'Height should be a number, got ' + (typeof height);
+		return;
+	}
+
+	return {
+		type: "WINDOW_HEIGHT_UPDATED",
+		payload: height
+	}
 };
