d7f1f4893db497eb0bcbce27db3367d7a676c0f1
jcrademacher <johnjr5599@gmail.com>
2017-03-27 22:19:07 -0400

    added util bar at top of MyRYLA, added profile support

--- dev/js/components/containers/MyRYLA.jsx
+++ dev/js/components/containers/MyRYLA.jsx
@@ -1,31 +1,46 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
 import { browserHistory } from 'react-router';
+import AccountNav from '../components/AccountNav.jsx';
 
-export default class MyRYLA extends React.Component {
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+
+import { setProfile } from '../../actions';
+
+// sends store state into AppView as props
+function mapStateToProps(state) {
+	return {
+		profile: state.profile,
+	};
+}
+
+// links actions with redux to be available as props in AppView
+function mapDispatchToProps(dispatch) {
+	return bindActionCreators({setProfile}, dispatch);
+}
+
+class MyRYLA extends React.Component {
 	constructor(props) {
 		super(props);
 
-		this.props.auth.on('profile_updated', () => this.setState({ name: this.props.auth.getProfile().user_metadata.name }));
+		// when AuthService emits profile updated event, update redux
+		this.props.auth.on('profile_updated', () => {
+			this.props.setProfile(this.props.auth.getProfile());
+		});
 
-		this.state = {
-			name: this.props.auth.getProfile().user_metadata.name
-		}
-	}
-
-	logout() {
-		this.props.auth.logout();
-
-		browserHistory.replace('/login');
+		this.props.setProfile(this.props.auth.getProfile());
 	}
 
 	render() {
+		const name = this.props.profile == null ? '' : this.props.profile.user_metadata.name;
+
 		return (
 			<div>
 				<br/><br/>
-				<h1>{this.state.name}</h1>
-				<Button onClick={() => this.logout()}>Logout</Button>
+				<AccountNav auth={this.props.auth} name={name}/>
 			</div>
 		);
 	}
 }
+
+export default connect(mapStateToProps, mapDispatchToProps)(MyRYLA);
