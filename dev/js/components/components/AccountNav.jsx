10fc5cb8ddeb70d318289d5ed9ddcc3f324e7a42
jcrademacher <johnjr5599@gmail.com>
2017-04-03 20:17:39 -0400

    fixed nav

--- dev/js/components/components/AccountNav.jsx
+++ dev/js/components/components/AccountNav.jsx
@@ -2,16 +2,12 @@ import React, { PropTypes as T} from 'react';
 import { Link } from 'react-router';
 
 const AccountNav = (props) => {
-	const divStyle = {
-		//border: '1px solid black',
-		marginRight: 26,
-		marginLeft: 26,
-	}
 
 	return (
-		<div style={divStyle}>
+		<div>
 			<br/>
-			<span style={{textAlign: "right"}}>
+			<strong>MyRYLA</strong>
+			<span style={{float: "right"}}>
 				Welcome, {props.name}! &emsp;
 				<Link style={{color: 'gray'}} to='/login' onClick={() => props.auth.logout()}>Sign out</Link>
 			</span>
@@ -21,7 +17,8 @@ const AccountNav = (props) => {
 }
 
 AccountNav.propTypes = {
-	auth: T.object.isRequired
+	auth: T.object.isRequired,
+	name: T.string.isRequired
 }
 
 export default AccountNav;
