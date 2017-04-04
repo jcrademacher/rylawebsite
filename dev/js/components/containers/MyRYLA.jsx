03549e02f98a129a2a9666b02d41c891e13d6a32
jcrademacher <johnjr5599@gmail.com>
2017-04-03 20:18:40 -0400

    minor

--- dev/js/components/containers/MyRYLA.jsx
+++ dev/js/components/containers/MyRYLA.jsx
@@ -7,40 +7,62 @@ import {bindActionCreators} from 'redux';
 
 import { setProfile } from '../../actions';
 
+import { Route, IndexRoute } from 'react-router';
+
+import Panel from '../components/Panel.jsx';
+
 // sends store state into AppView as props
 function mapStateToProps(state) {
 	return {
 		profile: state.profile,
+		windowWidth: state.windowWidth,
+		windowHeight: state.windowHeight
 	};
-}
-
-// links actions with redux to be available as props in AppView
-function mapDispatchToProps(dispatch) {
-	return bindActionCreators({setProfile}, dispatch);
 }
 
 class MyRYLA extends React.Component {
 	constructor(props) {
 		super(props);
 
-		// when AuthService emits profile updated event, update redux
-		this.props.auth.on('profile_updated', () => {
-			this.props.setProfile(this.props.auth.getProfile());
-		});
+		this.profile = props.auth.getProfile();
 
-		this.props.setProfile(this.props.auth.getProfile());
+		this.state = {
+			componentReady: this.profile != null
+		}
+
+		// when AuthService emits profile updated event, update redux
+		props.auth.on('profile_updated', () => {
+			this.profile = props.auth.getProfile();
+			this.setState({componentReady: true});
+		});
 	}
 
 	render() {
-		const name = this.props.profile == null ? '' : this.props.profile.user_metadata.name;
-
-		return (
+		if(this.state.componentReady) {
+			return (
+				<div style={{ marginRight: 26, marginLeft: 26 }}>
+					<br/><br/>
+					<AccountNav auth={this.props.auth} name={this.profile.user_metadata.name}/>
+					<Panel
+						textColor='black'
+						backgroundColor='rgb(156, 212, 228)'
+						shadow
+					>
+						{React.cloneElement(this.props.children, {
+							profile: this.profile,
+							windowWidth: this.props.windowWidth,
+							windowHeight: this.props.windowHeight})}
+					</Panel>
+				</div>
+			);
+		}
+		else return (
 			<div>
 				<br/><br/>
-				<AccountNav auth={this.props.auth} name={name}/>
+				<h2>Loading...</h2>
 			</div>
 		);
 	}
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(MyRYLA);
+export default connect(mapStateToProps)(MyRYLA);
