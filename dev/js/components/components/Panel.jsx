bd265442e99aa43b5ac8ba5e798dd84274b4ced8
jcrademacher <johnjr5599@gmail.com>
2017-04-03 20:18:08 -0400

    minor

--- dev/js/components/components/Panel.jsx
+++ dev/js/components/components/Panel.jsx
@@ -5,7 +5,7 @@ const Panel = (props) => {
 	const border = props.borderWidth == undefined ? 'none' : props.borderWidth + "px solid " + props.borderColor;
 
 	var backgroundColor = props.backgroundColor == undefined ? "none" : props.backgroundColor;
-	var shadow = props.shadow == undefined || props.shadow == false ? "none" : "10px 10px 15px rgb(130, 130, 130)";
+	var shadow = props.shadow == undefined || props.shadow == false ? "none" : "5px 5px 15px rgb(130, 130, 130)";
 
 	const panelStyle = {
 		backgroundColor: backgroundColor,
