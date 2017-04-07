ea2daacd990ab1b34ab08fe6fb0fc2b488b9c589
jcrademacher <johnjr5599@gmail.com>
2017-04-06 22:21:11 -0400

    made AccountNav more reusable, accepts titles and urls props

--- dev/js/components/components/AccountNav.jsx
+++ dev/js/components/components/AccountNav.jsx
@@ -1,16 +1,52 @@
 import React, { PropTypes as T} from 'react';
-import { Link } from 'react-router';
+import { Link, browserHistory } from 'react-router';
+import { DropdownButton, MenuItem } from 'react-bootstrap';
+
+const MLink = (props) => <Link style={{color: 'black', fontSize: 16}} to={props.to}>{props.children}</Link>;
 
 const AccountNav = (props) => {
+
+	// returns array of navitems with correct title and url from props
+	const iterateTitles = () => {
+		return props.titles.map((title, index) => {
+			if(!props.collapse)
+				return <span id={index}><MLink to={props.urls[index]}>{title}</MLink> &emsp;</span>;
+			else
+				return <MenuItem id={index} onClick={() => browserHistory.push(props.urls[index])}>{title}</MenuItem>
+		});
+	};
+
+	const getNavbar = () => {
+		if(!props.collapse) {
+			return (
+				<span>
+					<strong><MLink to='/MyRYLA'>MyRYLA</MLink></strong> &emsp;
+					{iterateTitles()}
+				</span>
+			);
+		}
+		else {
+			return (
+				<span>
+					<DropdownButton id='accountnav' title="MyRYLA">
+						{iterateTitles()}
+					</DropdownButton>
+				</span>
+			);
+		}
+	};
 
 	return (
 		<div>
 			<br/>
-			<strong>MyRYLA</strong>
-			<span style={{float: "right"}}>
+			<span style={{float: 'left'}}>
+				{getNavbar()}
+			</span>
+			<span style={{float: "right", fontSize: 16}}>
 				Welcome, {props.name}! &emsp;
 				<Link style={{color: 'gray'}} to='/login' onClick={() => props.auth.logout()}>Sign out</Link>
 			</span>
+			<br/>
 			<hr/>
 		</div>
 	);
