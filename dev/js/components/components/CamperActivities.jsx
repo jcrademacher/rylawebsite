4b91f14d5b06858422fe2029627a20da12f78145
jcrademacher <johnjr5599@gmail.com>
2017-04-09 21:49:31 -0400

    renamed Activities

---
+++ dev/js/components/components/CamperActivities.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class CamperActivities extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div>
+				<h2>Activities</h2>
+				<hr/>
+			</div>
+		);
+	}
+}
