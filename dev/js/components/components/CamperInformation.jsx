05601018fac3aee1fe9f878efbfd8df5433626c8
jcrademacher <johnjr5599@gmail.com>
2017-04-09 21:49:47 -0400

    renamed Information

---
+++ dev/js/components/components/CamperInformation.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class CamperInformation extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div>
+				<h2>Additional Information</h2>
+				<hr/>
+			</div>
+		);
+	}
+}
